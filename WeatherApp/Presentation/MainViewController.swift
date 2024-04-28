3821b78f38a1248af58f8f09508786dcc2957f21
aisaev <aisaev@azbukavkusa.ru>
2024-04-28 15:56:52 +0300

    Настройка проекта

---
+++ WeatherApp/Presentation/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  WeatherApp
+//
+//  Created by arslanali on 28.04.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		view.backgroundColor = .red
+    }
+  
+
+}
